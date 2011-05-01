d4bcd886f0836645572fb66d5646cf779fd280ef
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2011-05-01 17:37:47 -0400

    fixed bug in util.followers_for_object and renamed followers() to get_followers()

--- follow/util.py
+++ follow/util.py
@@ -3,24 +3,23 @@ from django.db.models.fields.related import ManyToManyField, ForeignKey
 registry = []
 model_map = {}
 
-def followers_for_object(self):
-    from models import Follower
+def get_followers_for_object(self):
+    from models import Follow
     return Follow.objects.get_followers_for_object(self)
 
 def register(model, field_name = None, m2m = False):
     """
-    This registers the model class so it can have followers
+    This registers the model class so it can have followers,
+    adds a method to the model ``get_followers``,
+    returning the followers - 
+    a query set of ``:class:auth.models.User`` objects
     """
-    from models import Follow
     if model in registry:
         return
         
     registry.append(model)
     
     related_name = 'follow_%s' % model._meta.module_name
-    
-    if not field_name:
-        field_name = model._meta.module_name
     
     # Create foreignkeys by default - less sql queries for lookups
     if m2m:
@@ -36,8 +35,13 @@ def register(model, field_name = None, m2m = False):
             null = True,
         )
     
+    if not field_name:
+        field_name = model._meta.module_name
+
+    from models import Follow
     field.contribute_to_class(Follow, field_name)
-    setattr(model, 'followers', followers_for_object)
-    
+
+    model.add_to_class('get_followers', get_followers_for_object)
+
     # We need to keep track of which fields and which kind of fields point where
     model_map[model] = [related_name, field_name, m2m]
